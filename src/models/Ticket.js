c15abb94311b24b9f1d8690c3f699f794f7cbccc
jyotiprakash swain <jp.swainabc@gmail.com>
2026-02-11 23:02:50 +0530

    new update

--- src/models/Ticket.js
+++ src/models/Ticket.js
@@ -1,35 +1,55 @@
 import mongoose from "mongoose";
 
+const StatusHistorySchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      enum: ["OPEN", "MARKED_DOWN", "RESOLVED"],
+      required: true,
+    },
+    changedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    changedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { _id: false }
+);
+
 const TicketSchema = new mongoose.Schema(
   {
     title: {
       type: String,
       required: true,
     },
+
     descriptionMarkdown: {
       type: String,
       required: true,
     },
+
     images: {
       type: [String],
       default: [],
     },
+
     status: {
       type: String,
       enum: ["OPEN", "MARKED_DOWN", "RESOLVED"],
       default: "OPEN",
     },
+
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
     },
-    markedDownBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    markedDownAt: {
-      type: Date,
+
+    statusHistory: {
+      type: [StatusHistorySchema],
+      default: [],
     },
   },
   { timestamps: true }
